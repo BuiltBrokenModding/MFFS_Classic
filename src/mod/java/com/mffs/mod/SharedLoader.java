de62bc441139383108006945cae755c6bd13e0ed
Phil <pwalne12@gmail.com>
2016-05-26 13:33:02 -0400

    Seemed git didn't want to add the files.

---
+++ src/mod/java/com/mffs/mod/SharedLoader.java
@@ -0,0 +1,37 @@
+package com.mffs.mod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by pwaln on 5/22/2016.
+ */
+public class SharedLoader {
+
+    /**
+     * Called before the main INITIALIZE.
+     * @param event Forge ModLoader event.
+     */
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    /**
+     * Called along with the main Initialize.
+     * @param event Forge ModLoader event.
+     */
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+    }
+
+    /**
+     * Called after the main Init.
+     * @param event Forge ModLoader event.
+     */
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
