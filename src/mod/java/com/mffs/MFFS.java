652f65820ebe13defa01272f58cab3bd39effd76
Phil <pwalne12@gmail.com>
2016-05-27 19:56:04 -0400

    Added all the textures from the old mod.

--- src/mod/java/com/mffs/MFFS.java
+++ src/mod/java/com/mffs/MFFS.java
@@ -12,7 +12,7 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 @Mod(modid = MFFS.MODID, name = MFFS.MOD_NAME, version = MFFS.VERSION)
 public class MFFS
 {
-    public static final String MODID = "modular_forcefield";
+    public static final String MODID = "mffs";
     public static final String VERSION = "1.0";
     public static final String MOD_NAME = "Modular_Forcefields";
 
