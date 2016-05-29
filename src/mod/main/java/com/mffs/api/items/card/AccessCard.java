c2b9ddb3e0c220028849971f9949ee76c233df14
Phil <pwalne12@gmail.com>
2016-05-29 18:26:18 -0400

    Converted the US eng lang file

--- src/mod/main/java/com/mffs/api/items/card/AccessCard.java
+++ src/mod/main/java/com/mffs/api/items/card/AccessCard.java
@@ -2,7 +2,11 @@ package com.mffs.api.items.card;
 
 import com.mffs.MFFS;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 /**
  * Created by pwaln on 5/29/2016.
@@ -20,5 +24,16 @@ public class AccessCard extends Item {
         this.itemIcon = p_94581_1_.registerIcon(MFFS.MODID+":AccessCard");
     }
 
+    /**
+     * allows items to add custom lines of information to the mouseover description
+     *
+     * @param stack
+     * @param usr
+     * @param tooltip
+     * @param dummy
+     */
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer usr, List tooltip, boolean dummy) {
 
+    }
 }
--- src/mod/main/java/com/mffs/api/items/card/PersonalIDCard.java
+++ src/mod/main/java/com/mffs/api/items/card/PersonalIDCard.java
@@ -1,8 +1,15 @@
 package com.mffs.api.items.card;
 
 import com.mffs.MFFS;
+import com.mffs.api.ItemManager;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.lwjgl.input.Keyboard;
+
+import java.util.List;
 
 /**
  * Created by pwaln on 5/29/2016.
@@ -20,5 +27,22 @@ public class PersonalIDCard extends Item {
         this.itemIcon = p_94581_1_.registerIcon(MFFS.MODID+":PersonalIDCard");
     }
 
-
+    /**
+     * allows items to add custom lines of information to the mouseover description
+     *
+     * @param stack
+     * @param usr
+     * @param tooltip
+     * @param dummy
+     */
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer usr, List tooltip, boolean dummy) {
+        tooltip.add(String.format("Owner: %s", ItemManager.getTag(stack).getString("name")));
+        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+                || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+            tooltip.add(LanguageRegistry.instance().getStringLocalization("itemInfo.rights"));
+        } else {
+            tooltip.add(LanguageRegistry.instance().getStringLocalization("itemInfo.rightsHoldShift"));
+        }
+    }
 }
