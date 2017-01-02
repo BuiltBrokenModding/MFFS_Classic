8755fc9be74a182ca1935d4612349c8ba2f53bbd
Phil <pwalne12@gmail.com>
2017-01-02 17:03:41 -0500

    Setting adjustments

--- src/main/java/com/mffs/SettingConfiguration.java
+++ src/main/java/com/mffs/SettingConfiguration.java
@@ -19,7 +19,7 @@ public class SettingConfiguration {
     public static boolean HIGH_GRAPHICS = true;
     public static int INTERDICTION_MURDER_ENERGY = 0;
     public static int INTERDICTION_MAX_RANGE = Integer.MAX_VALUE;
-    public static boolean ENABLE_MANIPULATOR = true;
+    public static boolean ENABLE_MANIPULATOR = false;
     public static short FORTRON_SYNC_TICKS = 60; // 3 seconds
 
     /* This is the base power required for the coercion deriver */
@@ -34,38 +34,47 @@ public class SettingConfiguration {
     public static void load() {
         Configuration CONFIGURATION = ModularForcefieldSystem.modularForcefieldSystem_mod.getConfig();
 
+        //GENERAL
         ENABLE_MANIPULATOR = CONFIGURATION.get("general", "Enable Force Manipulator", ENABLE_MANIPULATOR).getBoolean(ENABLE_MANIPULATOR);
-
-        FORTRON_PRODUCTION_MULTIPLIER = (float) CONFIGURATION.get("general", "Fortron Production Multiplier", FORTRON_PRODUCTION_MULTIPLIER).getDouble(FORTRON_PRODUCTION_MULTIPLIER);
-
-        Property propFieldScale = CONFIGURATION.get("general", "Max Force Field Scale", MAX_FORCE_FIELD_SCALE);
-        MAX_FORCE_FIELD_SCALE = propFieldScale.getInt(MAX_FORCE_FIELD_SCALE);
-
-        Property propCreative = CONFIGURATION.get("general", "Effect Creative Players", INTERACT_CREATIVE);
-        propCreative.comment = "Should the interdiction matrix interact with creative players?.";
-        INTERACT_CREATIVE = propCreative.getBoolean(INTERACT_CREATIVE);
-
         Property propChunkLoading = CONFIGURATION.get("general", "Load Chunks", LOAD_CHUNKS);
         propChunkLoading.comment = "Set this to false to turn off the ModularForcefieldSystem Chunkloading capabilities.";
         LOAD_CHUNKS = propChunkLoading.getBoolean(LOAD_CHUNKS);
+        DEV_MODE = CONFIGURATION.getBoolean("Debug Mode", "general", DEV_MODE, "This is for developers!");
 
         Property propOpOverride = CONFIGURATION.get("general", "Op Override", OP_OVERRIDE);
         propOpOverride.comment = "Allow the operator(s) to override security measures created by ModularForcefieldSystem?";
         OP_OVERRIDE = propOpOverride.getBoolean(OP_OVERRIDE);
 
+        //FORTRON
+        FORTRON_PRODUCTION_MULTIPLIER = (float) CONFIGURATION.get("fortron", "Fortron Production Multiplier", FORTRON_PRODUCTION_MULTIPLIER).getDouble(FORTRON_PRODUCTION_MULTIPLIER);
+        Property useElectricity = CONFIGURATION.get("fortron", "Require Electricity?", ENABLE_ELECTRICITY);
+        useElectricity.comment = "Turning this to false will make ModularForcefieldSystem run without electricity or energy systems required. Great for vanilla!";
+        ENABLE_ELECTRICITY = useElectricity.getBoolean(ENABLE_ELECTRICITY);
+        INTERDICTION_MURDER_ENERGY = CONFIGURATION.get("fortron", "Interdiction Murder Fortron Consumption", INTERDICTION_MURDER_ENERGY).getInt(INTERDICTION_MURDER_ENERGY);
+        BASE_POWER_REQUIRED = CONFIGURATION.get("fortron", "Base Coercion Deriver Power Consumption", BASE_POWER_REQUIRED).getInt(BASE_POWER_REQUIRED);
 
-        Property maxFFGenPerTick = CONFIGURATION.get("general", "Field Calculation Per Tick", MAX_FORCE_FIELDS_PER_TICK);
+        //FORCEFIELD_PROJECTOR
+        Property propFieldScale = CONFIGURATION.get("forcefield_projector", "Max Force Field Scale", MAX_FORCE_FIELD_SCALE);
+        MAX_FORCE_FIELD_SCALE = propFieldScale.getInt(MAX_FORCE_FIELD_SCALE);
+
+        Property maxFFGenPerTick = CONFIGURATION.get("forcefield_projector", "Field Calculation Per Tick", MAX_FORCE_FIELDS_PER_TICK);
         maxFFGenPerTick.comment = "How many force field blocks can be generated per tick? Less reduces lag.";
         MAX_FORCE_FIELDS_PER_TICK = maxFFGenPerTick.getInt(MAX_FORCE_FIELDS_PER_TICK);
 
-        Property interdictionRange = CONFIGURATION.get("general", "Field Calculation Per Tick", INTERDICTION_MAX_RANGE);
+        //INTERDICTION_MATRIX
+        Property interdictionRange = CONFIGURATION.get("interdiction_matrix", "Field Calculation Per Tick", INTERDICTION_MAX_RANGE);
         interdictionRange.comment = "The maximum range for the interdiction matrix.";
         INTERDICTION_MAX_RANGE = interdictionRange.getInt(INTERDICTION_MAX_RANGE);
 
-        Property useElectricity = CONFIGURATION.get("general", "Require Electricity?", ENABLE_ELECTRICITY);
-        useElectricity.comment = "Turning this to false will make ModularForcefieldSystem run without electricity or energy systems required. Great for vanilla!";
-        ENABLE_ELECTRICITY = useElectricity.getBoolean(ENABLE_ELECTRICITY);
+        Property propCreative = CONFIGURATION.get("interdiction_matrix", "Effect Creative Players", INTERACT_CREATIVE);
+        propCreative.comment = "Should the interdiction matrix interact with creative players?.";
+        INTERACT_CREATIVE = propCreative.getBoolean(INTERACT_CREATIVE);
 
+        Property anti_personel = CONFIGURATION.get("interdiction_matrix", "Require Collection module", COLLECT_ON_PERSONELL_KILL);
+        anti_personel.comment = "Set to true for interdiction matrix to collect items from killed players without collection module.";
+        COLLECT_ON_PERSONELL_KILL = anti_personel.getBoolean(COLLECT_ON_PERSONELL_KILL);
+
+        //MODULES
         Property forceManipulatorBlacklist = CONFIGURATION.get("modules", "Force Manipulator Blacklist", "");
         forceManipulatorBlacklist.comment = "Put a list of block IDs to be not-moved by the force manipulator. Separate by commas, no space.";
         String blackListManipulate = forceManipulatorBlacklist.getString();
@@ -79,16 +88,6 @@ public class SettingConfiguration {
         String blackListDisintegrate = blacklist1.getString();
         //Blacklist.disintegrationBlacklist.addAll(LanguageUtility.decodeIDSeparatedByComma(blackListDisintegrate));
 
-        Property anti_personel = CONFIGURATION.get("modules", "Anti-Personell_collect", COLLECT_ON_PERSONELL_KILL);
-        anti_personel.comment = "Set to true for interdiction matrix to collect items from killed players without confiscate module";
-        COLLECT_ON_PERSONELL_KILL = anti_personel.getBoolean(COLLECT_ON_PERSONELL_KILL);
-
-        INTERDICTION_MURDER_ENERGY = CONFIGURATION.get("energy", "Interdiction Murder Fortron Consumption", INTERDICTION_MURDER_ENERGY).getInt(INTERDICTION_MURDER_ENERGY);
-        FORTRON_PRODUCTION_MULTIPLIER = (float) CONFIGURATION.get("energy", "Fortron Production Multiplier", FORTRON_PRODUCTION_MULTIPLIER).getDouble(FORTRON_PRODUCTION_MULTIPLIER);
-        BASE_POWER_REQUIRED = CONFIGURATION.get("energy", "Base Coercion Deriver Power Consumption", BASE_POWER_REQUIRED).getInt(BASE_POWER_REQUIRED);
-
-        DEV_MODE = CONFIGURATION.getBoolean("Debug Mode", "Configuration", DEV_MODE, "This is for developers!");
-        System.out.println(DEV_MODE);
         CONFIGURATION.save();
 
     }
--- src/main/java/com/mffs/common/items/modules/interdiction/ItemModuleAntiPersonnel.java
+++ src/main/java/com/mffs/common/items/modules/interdiction/ItemModuleAntiPersonnel.java
@@ -5,6 +5,7 @@ import com.mffs.api.security.IBiometricIdentifier;
 import com.mffs.api.security.IInterdictionMatrix;
 import com.mffs.api.security.Permission;
 import com.mffs.common.items.modules.MatrixModule;
+import com.mffs.common.items.modules.projector.ItemModuleCollection;
 import com.mffs.common.items.modules.projector.ItemModuleShock;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.entity.EntityLivingBase;
@@ -37,7 +38,7 @@ public class ItemModuleAntiPersonnel extends MatrixModule {
             EntityPlayer pl = (EntityPlayer) paramEntityLivingBase;
             IBiometricIdentifier bio = matri.getBiometricIdentifier();
             if (!pl.capabilities.isCreativeMode && !pl.isEntityInvulnerable() && bio.isAccessGranted(pl.getGameProfile().getName(), Permission.BYPASS_DEFENSE)) {
-                if (SettingConfiguration.COLLECT_ON_PERSONELL_KILL) {
+                if (SettingConfiguration.COLLECT_ON_PERSONELL_KILL || matri.getModuleCount(ItemModuleCollection.class) > 0) {
                     Set<ItemStack> safe_items = matri.getFilteredItems();
                     for (int slot = 0; slot < pl.inventory.getSizeInventory(); slot++) {
                         ItemStack stack = pl.inventory.getStackInSlot(slot);
