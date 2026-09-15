ea2ea74257b670566fe94642433b56d072043181
Bknibb <bailey.knibb@gmail.com>
2026-09-15 23:06:16 +1000

    Performance patch (1.21.1) (#142)

--- src/main/java/dev/su5ed/mffs/render/ForceFieldBlockEntityRenderer.java
+++ src/main/java/dev/su5ed/mffs/render/ForceFieldBlockEntityRenderer.java
@@ -5,7 +5,9 @@ import dev.su5ed.mffs.blockentity.ForceFieldBlockEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public class ForceFieldBlockEntityRenderer implements BlockEntityRenderer<ForceFieldBlockEntity> {
 
@@ -17,5 +19,10 @@ public class ForceFieldBlockEntityRenderer implements BlockEntityRenderer<ForceF
         if (camouflage != null) {
             BlockEntityRenderDelegate.INSTANCE.tryRenderDelegate(blockEntity, partialTick, poseStack, bufferSource, packedLight, packedOverlay);
         }
+    }
+
+    @Override
+    public boolean shouldRender(ForceFieldBlockEntity blockEntity, Vec3 cameraPos) {
+        return blockEntity.getCamouflage() != null && blockEntity.getCamouflage().getBlock() instanceof EntityBlock && BlockEntityRenderer.super.shouldRender(blockEntity, cameraPos);
     }
 }
